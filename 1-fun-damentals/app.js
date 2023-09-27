c5ceaae5022a186bce6727cc1980fc435a556c9d
Azmi Alfoghi <drama.com.aa@gmail.com>
2023-09-27 21:53:16 +0200

    modified:   1-fun-damentals/app.js

---
+++ 1-fun-damentals/app.js
@@ -0,0 +1,39 @@
+// values and Variables
+// let firstName = 'Azmi';
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// let PI = 3.1415; 
+// DataTypes
+// const isTrue: boolean = true; // Boolean (true/false)
+// const age : number=20;// Number
+// const name : string='Azmi' ;   /// String
+// const address : any ='Bogor, Indonesia ';    //// Any
+// const hobbies : Array<string>=['Sports', 'Music'];     //// Arrays of strings
+// const roles : Roles[]=[{id:'admin'}, {id:'user'}];      //// Objects with an array inside it
+
+// vlaue
+// OBJECTS
+// let me = {
+//     name: 'Azmi',
+//     age: 27,
+// }
+// or
+// PRIMITIVE
+// let firstName = 'Azmi';
+// let age = 27;
+// console.log('Name:',me?.name,'Age:',age );
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun)
+// console.log(typeof javascriptIsFun)
+// console.log(typeof false)
+// console.log(typeof 29)
+// console.log(typeof null) // BUG 
+
+// let, const and var
+let age = 26;
+age = 27;
+
+const birthYear = 1996;
+
